7f3171d52cf016fabeaa74eb9e1aa2f535dcbd46
Sunyeop Lee <slee@squarelab.co>
2019-01-07 17:12:51 +0900

    Add passthru, read_decode, print_, pprint
    - Fix weather test
    - Add _composite()

--- test.py
+++ test.py
@@ -1,18 +1,22 @@
+import json
 import logging
 
 from declrest import endpoint, GET, query, header, f, json_decode, decode, \
-    findall, retmap
+    findall, retmap, read_decode, passthru, print_, pprint
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
 
+    # using read_decode()
     @endpoint('https://api.ipify.org')
-    @decode()
+    @read_decode()
     def get_my_ip(params):
         pass
 
+    # using findall, retmap
     @endpoint('http://whatsmyuseragent.org')
     @header('User-Agent', f('{my_user_agent}'))
+    @read_decode()
     @findall(r'user-agent.*\s*.*intro-text.*?>([^<]*)')
     @retmap(lambda r: r[0])
     def get_my_user_agent(my_user_agent='DeclREST/1.0', params=None):
@@ -20,12 +24,17 @@ if __name__ == '__main__':
         # params.headers['User-Agent'] = my_user_agent
         pass
 
-    @endpoint('http://samples.openweathermap.org')
+    # using passthru, json_decode, print_ and pprint
+    @endpoint('https://samples.openweathermap.org')
     @GET('/data/2.5/weather')
     @query('q', 'London,uk')
     @query('appid', 'b6907d289e10d714a6e88b30761fae22')
     @header('User-Agent', 'DeclREST/1.0')
+    @passthru(lambda resp: print(resp.headers))
+    @read_decode()
+    @print_()
     @json_decode()
+    @pprint(indent=2)
     def get_weather(params):
         params.headers['X-Powered-By'] = 'declrest'
         print('params:', params)
@@ -48,6 +57,7 @@ if __name__ == '__main__':
     print(get_weather())
     print(get_weather2())
 
+    # various formatting methods
     @endpoint(f('https://{cls.__name__}.com/{user_id}/{repo}'))
     class Repo:
         @classmethod
